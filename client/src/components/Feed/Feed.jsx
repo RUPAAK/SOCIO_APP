c981b7536ee6593f0553674ab0fe6fede01be124
Rupak <rupakt525@gmail.com>
2021-06-06 16:58:57 +0545

    post done and dusted

--- client/src/components/Feed/Feed.jsx
+++ client/src/components/Feed/Feed.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Post from '../../components/Post/Post'
 import './Feed.css'
 
 const Feed = () => {
     return (
         <div className="feed_main_container">
-            hi
+            <div className="feed_wrapper">
+                <Post />
+            </div>
         </div>
     )
 }
---
+++ client/src/components/Post/Post.jsx
@@ -0,0 +1,34 @@
+import './Post.css'
+import {PermMedia, EmojiEmotions, LocationOn} from '@material-ui/icons'
+
+const Post = () => {
+    return (
+        <div className="post_main_container">
+            <div className="post_container">
+                <div className="top_part">
+                    <img src="/uploads/c.jpeg" alt="" className="postProfile" />
+                    <input type="text" placeholder="What's on your mind" className="postInput" />
+                </div>
+                <hr/>
+                <div className="bottom_part">
+                    <div className="postOptions">
+                        <div className="postOptionsIcons">
+                            <PermMedia htmlColor="coral" className="postIcon" />
+                            <span className="postIconDetail">Photos/Video</span>
+                        </div>
+                        <div className="postOptionsIcons">
+                            <LocationOn htmlColor="lightblue" className="postIcon" />
+                            <span className="postIconDetail">Location</span>
+                        </div>
+                        <div className="postOptionsIcons">
+                            <EmojiEmotions htmlColor="greenyellow" className="postIcon" />
+                            <span className="postIconDetail">Feelings</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Post
