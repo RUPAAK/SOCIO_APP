679f80393c2c22fc7e67e77b9cc74dece1cc47db
Rupak <rupakt525@gmail.com>
2021-06-07 21:23:41 +0545

    posts done

--- client/src/components/News/News.jsx
+++ client/src/components/News/News.jsx
@@ -1,36 +1,48 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './News.css'
 import {MoreVert, ThumbUpAlt, ChatBubble, Share } from '@material-ui/icons'
+import {Users} from '../../dummyData'
 
-const News = () => {
+const News = ({user}) => {
+    const thisUser= Users.filter(x=> x.id === user.userId)
+    const [likes, setlikes] = useState(user.like)
+    const [testLiked, settestLiked] = useState(false)
+
+    const onClickHandler=()=>{
+        if(testLiked){
+            setlikes(likes-1)
+        }else{
+            setlikes(likes+1)
+        }
+        settestLiked(!testLiked)
+    }
     return (
         <>
             <div className="news_main_container">
                 <div className="news_container">
                     <div className="top_news_container">
                         <div className="top_news_left_container">
-                            <img src="/uploads/d.jpg" className="news_profile" alt="profile" />
-                            <span className="news_user">Rupak Thapa Magar</span>
-                            <span className="news_time">5 days ago</span>
+                            <img src={thisUser[0].profilePicture} className="news_profile" alt="profile" />
+                            <span className="news_user">{thisUser[0].username}</span>
+                            <span className="news_time">{user.date}</span>
                         </div>
                             <MoreVert />
                     </div>
                     <div className="middle_news_container">
-                        <span className="news_caption">Lhello all</span>
-                        <img src="/uploads/post.jpg" alt="" className="news_image" />
+                        <span className="news_caption">{user.desc}</span>
+                        <img src={user.image} alt="" className="news_image" />
                     </div>
                     <div className="right_news_container">
                         <div className="like_container">
                             <div className="like_count_container">
                                 <img src="/uploads/like.png" alt="" className="like" />
-                                <img src="/uploads/haha.png" alt="" className="like" />
-                                <span className="like_count">25 people likes it</span>
+                                <span className="like_count" > {likes} pople like this</span>
                             </div>
-                            <span className="comments">9 comments</span>
+                            <span className="comments">{user.comment} comments</span>
                         </div>
                         <hr/>
                         <div className="add_like_comment_container">
-                            <span className="like_comment"><ThumbUpAlt htmlColor="blue" /></span>
+                            <span className="like_comment" onClick={onClickHandler}  ><ThumbUpAlt /></span>
                             <span className="like_comment"><ChatBubble htmlColor="grey"/></span>
                             <span className="like_comment"><Share htmlColor="darkgreen" /></span>
                         </div>
---
+++ client/src/dummyData.js
@@ -0,0 +1,106 @@
+const Posts= [
+    {
+        id: 1,
+        desc: "This is First Post",
+        image: "uploads/posts/b.jpeg",
+        date: "15 mins ago",
+        userId: 1,
+        like: 55,
+        comment: 12,
+    },
+    {
+        id: 2,
+        desc: "This is Second Post",
+        image: "uploads/posts/c.jpeg",
+        date: "2 Dats ago",
+        userId: 2,
+        like: 84,
+        comment: 0,
+    },
+    {
+        id: 3,
+        desc: "This is First Post",
+        image: "uploads/posts/d.jpg",
+        date: "35 mins ago",
+        userId: 3,
+        like: 37,
+        comment: 4,
+    },
+    {
+        id: 4,
+        desc: "This is First Post",
+        image: "uploads/posts/e.jpeg",
+        date: "4 mins ago",
+        userId: 4,
+        like: 8,
+        comment: 17,
+    },
+    {
+        id: 5,
+        desc: "This is Fifth Post",
+        image: "uploads/posts/f.jpg",
+        date: "35 mins ago",
+        userId: 5,
+        like: 572,
+        comment: 97,
+    },
+    {
+        id: 6,
+        desc: "This is First Post",
+        image: "uploads/posts/g.jpeg",
+        date: "6 mins ago",
+        userId: 6,
+        like: 77,
+        comment: 21,
+    },
+    {
+        id: 7,
+        desc: "This is last Post",
+        image: "uploads/posts/a.jpeg",
+        date: "30 mins ago",
+        userId: 7,
+        like: 32,
+        comment: 8,
+    }
+
+]
+
+const Users=[
+    {
+        id:1,
+        profilePicture: "uploads/users/a.jpeg",
+        username: "Rupak Thapa Magar",
+    },
+    {
+        id:2,
+        profilePicture: "uploads/users/b.jpeg",
+        username: "Hari Thapa",
+    },
+    {
+        id:3,
+        profilePicture: "uploads/users/c.jpeg",
+        username: "Surya Acharya",
+    },
+    {
+        id:4,
+        profilePicture: "uploads/users/d.jpg",
+        username: "Prakash BK",
+    },
+    {
+        id:5,
+        profilePicture: "uploads/users/e.jpeg",
+        username: "Gita Rai",
+    },
+    {
+        id:6,
+        profilePicture: "uploads/users/f.png",
+        username: "Pretti Majhi",
+    },
+    {
+        id:7,
+        profilePicture: "uploads/users/g.jpeg",
+        username: "Sakesh Tamang",
+    },
+]
+
+export {Posts, Users}
