9051436bfdaa323a8e5987208e26e7d220746f23
Rupak <rupakt525@gmail.com>
2021-06-08 19:07:33 +0545

    store setup

---
+++ client/src/actions/userActions.js
@@ -0,0 +1,21 @@
+import {USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL} from '../constants/userConstants'
+import axios from 'axios'
+
+export const userLoginAction= (email, password)=>async(dispatch)=>{
+    try {
+        dispatch({type: USER_LOGIN_REQUEST})
+        const {data}= await axios.post('http://localhost:5000/api/users/login', {email, password})
+
+        dispatch({
+            type: USER_LOGIN_SUCCESS,
+            payload: data
+        })
+
+        await localStorage.setItem('userDetails', JSON.stringify(data))
+    } catch (error) {
+        dispatch({
+            type: USER_LOGIN_FAIL,
+            payload: error.response && error.response.data.messge? error.resposne.data.message : error.response
+        })
+    }
+}
