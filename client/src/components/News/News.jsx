b98063e349065dd86e97d9f39a295c54635b8df9
Rupak <rupakt525@gmail.com>
2021-06-17 12:50:15 +0545

    friends section done

--- client/src/components/News/News.jsx
+++ client/src/components/News/News.jsx
@@ -1,20 +1,20 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import './News.css'
-import {MoreVert, ThumbUpAlt, ChatBubble, Share } from '@material-ui/icons'
-import {Link} from 'react-router-dom'
+import { MoreVert, ThumbUpAlt, ChatBubble, Share } from '@material-ui/icons'
+import { Link } from 'react-router-dom'
 import axios from 'axios'
 
-const News = ({post}) => {
+const News = ({ post }) => {
     const [userData, setuserData] = useState()
-    useEffect(()=>{
-        axios.get(`http://localhost:5000/api/posts/user/${post.userId}`).then((response)=>{
+    useEffect(() => {
+        axios.get(`http://localhost:5000/api/posts/user/${post.userId}`).then((response) => {
             setuserData(response.data)
-        }).catch((e)=> console.log(e))
-    }, [])  
+        }).catch((e) => console.log(e))
+    }, [])
 
     return (
         <>
-( <div className="news_main_container">
+            <div className="news_main_container">
                 <div className="news_container">
                     <div className="top_news_container">
                         <div className="top_news_left_container">
@@ -22,7 +22,7 @@ const News = ({post}) => {
                             <span className="news_user">{userData && userData.username}</span>
                             <span className="news_time">{post.createdAt}</span>
                         </div>
-                            <MoreVert />
+                        <MoreVert />
                     </div>
                     <div className="middle_news_container">
                         <span className="news_caption">{post.desc}</span>
@@ -36,10 +36,10 @@ const News = ({post}) => {
                             </div>
                             <span className="comments">9 comments</span>
                         </div>
-                        <hr/>
+                        <hr />
                         <div className="add_like_comment_container">
                             <span className="like_comment" ><ThumbUpAlt /></span>
-                            <span className="like_comment"><ChatBubble htmlColor="grey"/></span>
+                            <span className="like_comment"><ChatBubble htmlColor="grey" /></span>
                             <span className="like_comment"><Share htmlColor="darkgreen" /></span>
                         </div>
                     </div>
--- client/src/screens/Profile/Profile.jsx
+++ client/src/screens/Profile/Profile.jsx
@@ -1,31 +1,30 @@
 import Navbar from '../../components/Navbar/Navbar'
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import './Profile.css'
 import Post from '../../components/Post/Post'
 import News from '../../components/News/News'
-import {userPersonalPosts} from '../../actions/userActions'
-import {useDispatch, useSelector} from 'react-redux'
-import { Users} from '../../dummyData'
+import { userPersonalPosts } from '../../actions/userActions'
+import { useDispatch, useSelector } from 'react-redux'
+import { Users } from '../../dummyData'
 import axios from 'axios'
 
-const Profile = ({match}) => {
+const Profile = ({ match }) => {
     const [profile, setprofile] = useState('')
     const dispatch = useDispatch()
-        
+
     useEffect(() => {
-        axios.get(`http://localhost:5000/api/users/profile/${match.params.id}`).then((response)=>{
-            console.log(response.data)
+        axios.get(`http://localhost:5000/api/users/profile/${match.params.id}`).then((response) => {
             setprofile(response.data)
-    })
+        })
 
         dispatch(userPersonalPosts(match.params.id))
     }, [])
 
-    const userLoginDetails= useSelector(state=> state.userLoginDetails)
-    const {userDetails }= userLoginDetails
+    const userLoginDetails = useSelector(state => state.userLoginDetails)
+    const { userDetails } = userLoginDetails
 
-    const userPosts= useSelector(state=> state.userPosts)
-    const {posts, user}= userPosts
+    const userPosts = useSelector(state => state.userPosts)
+    const { posts, user } = userPosts
 
     return (
         <>
@@ -47,31 +46,31 @@ const Profile = ({match}) => {
                             <div className="friends_section">
                                 <p>FRIENDS</p>
                                 <div className="friends">
-                                {Users.map(user=>{
-                                    return(
-                                        <>
-                                            <span className="friend">
-                                                <img className="friend_pp" src={user.profilePicture} alt="" />
-                                            </span>
-                                        </>
-                                    )
-                                })}
+                                    {profile && profile.friends.map(user => {
+                                        return (
+                                            <>
+                                                <span className="friend">
+                                                    <img className="friend_pp" src={user.profilePicture} alt="" />
+                                                </span>
+                                            </>
+                                        )
+                                    })}
                                 </div>
                             </div>
                             <div className="feed_section">
                                 <div className="post_section">
-                                    <Post/>
+                                    <Post />
                                 </div>
                                 <div className="mypost_section">
-                                    {posts.posts.length == 0? (<h1>NO POSTS</h1>): (
-                                        posts.posts.map((post, index)=> <News key={index} post={post} user={posts.user}/>)
+                                    {posts.posts.length === 0 ? (<h1>NO POSTS</h1>) : (
+                                        posts.posts.map((post, index) => <News key={index} post={post} user={posts.user} />)
                                     )}
                                 </div>
                             </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
         </>
     )
 }
